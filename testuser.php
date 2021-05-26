4e861cfe052174b0afbce303fa10a36ca543a65d
sim001 <simon.amoyal4@gmail.com>
2021-05-26 09:46:36 +0200

    tp2 sans test

---
+++ testuser.php
@@ -0,0 +1,10 @@
+<?php
+
+include 'user.php';
+
+use PHPUnit\Framework\TestCase;
+
+class Testuser extends TestCase {
+
+
+}
---
+++ user.php
@@ -0,0 +1,7 @@
+<?php
+
+class User {
+    public $email;
+    public $nom;
+    public $age;
+}
