df159bd70036309a8733c48790d88e672458a538
sim001 <simon.amoyal4@gmail.com>
2021-05-26 11:24:16 +0200

    test

--- testuser.php
+++ testuser.php
@@ -7,4 +7,15 @@ use PHPUnit\Framework\TestCase;
 class Testuser extends TestCase {
 
 
+    private function testUserConstructor() : void {
+
+        $user = new User("simon.amoyal@icloud.com", "AMOYAL", 24);
+
+        //verifier les infos 
+        $this->assertEquals($user->getEmail(), "simon.amoyal@icloud.com");
+        $this->assertEquals($user->getName(), "AMOYAL");
+        $this->assertEquals($user->getAge(), 24);
+
+    }
+
 }
--- user.php
+++ user.php
@@ -1,7 +1,33 @@
 <?php
 
 class User {
-    public $email;
-    public $nom;
-    public $age;
+    private $email;
+    private $nom;
+    private $age;
+
+    public function __construct($email, $nom, $age)
+
+    {
+        $this->email = $email;
+        $this->nom = $nom;
+        $this->age = $age;
+    }
+
+    public function isEmailValid() : bool {
+        if(filter_var($this->getEmail(), FILTER_VALIDATE_EMAIL)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+    public function getAge() {
+        return $this->age;
+    }
+    public function getName() {
+        return $this->nom;
+    }
 }
